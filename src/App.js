284cfb5bec4c018766f4cc74182bbc8b7e261f66
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-21 23:12:57 +0600

    added dependencies and assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
     return (
         <div>
-            <h1 className="text-9xl">Hello world!</h1>
+            <h1 className="bg-primary text-4xl">Hello world!</h1>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+/* eslint-disable global-require */
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: '#c08d4c',
+                    secondary: '#c08c4d',
+                    accent: '#707070',
+                    black: '#2d2a2a',
+                    'base-100': '#ffffff',
+                },
+            },
+        ],
+    },
+    plugins: [require('daisyui')],
 };
