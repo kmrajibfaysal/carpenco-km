e2ccf92b4a90a75018a24fbc07783a64b559e07a
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-22 05:53:35 +0600

    fixed single product button

--- src/Pages/Home/SingleProduct.js
+++ src/Pages/Home/SingleProduct.js
@@ -5,9 +5,13 @@ function SingleProduct() {
     return (
         <div className="card w-96 rounded-none border-none font-josefin">
             <figure>
-                <img src={test} alt="Buttel" />
+                <img
+                    className="duration-400 transition-all ease-in-out hover:scale-110 "
+                    src={test}
+                    alt="Buttel"
+                />
             </figure>
-            <div className="card-body items-center justify-center">
+            <div className="card-body items-start justify-center">
                 <h2 className="card-title">Buttel</h2>
 
                 <div>
@@ -16,7 +20,7 @@ function SingleProduct() {
                     <p>Minimum order: 5</p>
                     <p>Available in stock: 32 </p>
                 </div>
-                <div className="card-actions justify-end">
+                <div className="flex items-start justify-start">
                     <button className="scale-75 border-2 border-primary bg-primary px-7 py-3 font-josefin text-base font-bold text-white transition duration-500 ease-in-out hover:border-primary  hover:bg-transparent hover:text-primary md:scale-90  lg:scale-100">
                         Place order
                     </button>
