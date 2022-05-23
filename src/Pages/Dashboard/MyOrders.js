52c09f3d638fa3297f7fb37976396c76e5de34bf
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-23 12:51:29 +0600

    added method to show user specific order in order page

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 function MyOrders() {
+    const [user] = useAuthState(auth);
+    const { email } = user;
+    const { isLoading, data: orders } = useQuery('orders', () =>
+        fetch(`http://localhost:5000/order/?email=${email}`).then((res) => res.json())
+    );
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <div>
             <h2 className="text-3xl">My orders</h2>
+            <p>{orders.length}</p>
         </div>
     );
 }
