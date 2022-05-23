a5b7d242ef00e91bf0d86e4074f84a070937a43b
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-23 06:44:35 +0600

    added method to show dynamic product data in home page

--- src/Pages/Home/SingleProduct.js
+++ src/Pages/Home/SingleProduct.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import test from '../../Assets/products/buttel.png';
 
-function SingleProduct() {
+function SingleProduct({ product }) {
+    const { name, sd, price, minOrder, stock, img } = product;
     return (
         <div className="card w-96 rounded-none border-none font-josefin">
             <figure>
                 <img
                     className="duration-400 transition-all ease-in-out hover:scale-110 "
-                    src={test}
+                    src={img}
                     alt="Buttel"
                 />
             </figure>
             <div className="card-body items-start justify-center">
-                <h2 className="card-title">Buttel</h2>
+                <h2 className="card-title">{name}</h2>
 
                 <div>
-                    <p>Professional buttel made from first class metal and wood.</p>
-                    <p>USD. 4.99$ per unit </p>
-                    <p>Minimum order: 5</p>
-                    <p>Available in stock: 32 </p>
+                    <p>{sd}</p>
+                    <p>USD. {price}$ per unit </p>
+                    <p>Minimum order: {minOrder}</p>
+                    <p>Available in stock: {stock} </p>
                 </div>
                 <div className="flex items-start justify-start">
                     <button className="border-2 border-primary bg-primary px-7 py-3 font-josefin text-base font-bold text-white transition duration-500 ease-in-out hover:border-primary  hover:bg-transparent hover:text-primary ">
