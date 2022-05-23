431c04a3c14b6183158679a20636443573a8d4cd
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-23 07:07:02 +0600

    added dynamic home page blog

--- src/Pages/Home/HomeBlog.js
+++ src/Pages/Home/HomeBlog.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 import SingleBlog from './SingleBlog';
 
 function HomeBlog() {
+    const { isLoading, data: blogs } = useQuery('blogs', () =>
+        fetch(`http://localhost:5000/blogs`).then((res) => res.json())
+    );
+
+    if (isLoading) return <Loading />;
+
     return (
         <div className="container mx-auto my-20 font-karla">
             <h4 className="text-md text-center font-josefin text-gray-400">
@@ -9,10 +17,9 @@ function HomeBlog() {
             </h4>
             <h4 className="text-center  text-4xl text-black">Carpenters Blog</h4>
             <div className="mx-auto mt-10 flex w-11/12 flex-col items-center justify-between space-x-3 md:flex-row">
-                <SingleBlog />
-                <SingleBlog />
-                <SingleBlog />
-                <SingleBlog />
+                {blogs.map((blog) => (
+                    <SingleBlog key={blog._id} blog={blog} />
+                ))}
             </div>
         </div>
     );
--- src/Pages/Home/SingleBlog.js
+++ src/Pages/Home/SingleBlog.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import blog1 from '../../Assets/Blogs/blog-1.png';
 
-function SingleBlog() {
+function SingleBlog({ blog }) {
+    const { title, submitted, img } = blog;
     return (
         <div className="max-w-xs overflow-hidden">
-            <img className="w-full" src={blog1} alt="Mountain" />
+            <img className="w-full" src={img} alt="Mountain" />
             <div className=" py-4">
-                <p className="mb-2 text-xs font-bold uppercase text-accent">
-                    by Ram M December 27, 2021
-                </p>
-                <p className=" text-lg font-bold text-black">
-                    A luxury custom home builder new home remodeling
-                </p>
+                <p className="mb-2 text-xs font-bold uppercase text-accent">{submitted}</p>
+                <p className=" text-lg font-bold text-black">{title}</p>
             </div>
             <div className=" pt-4 pb-2">
                 <button className=" border-2 border-gray-700 px-7 py-3 font-josefin text-base font-bold text-black transition duration-500 ease-in-out hover:bg-gray-700 hover:text-white">
