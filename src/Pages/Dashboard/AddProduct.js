866d3a6c618ad919897eff63eb7f5c0436def9c0
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-25 02:10:07 +0600

    added add product page with react hook form

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -1,9 +1,126 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function AddProduct() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <div>
-            <h2 className="text-3xl">Add Products</h2>
+            <h2 className="mt-16 text-center text-4xl">Add A Product</h2>
+            <form className="mx-auto mt-6 max-w-lg space-y-4" onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Product Name
+                    </label>
+                    <input
+                        {...register('name', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.name?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Details
+                    </label>
+                    <input
+                        {...register('sd', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.sd?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Price per unit
+                    </label>
+                    <input
+                        {...register('price', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        placeholder="4.99"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.price?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Minimum order quantity
+                    </label>
+                    <input
+                        {...register('minOrder', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.minOrder?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Stock
+                    </label>
+                    <input
+                        {...register('stock', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.stock?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Material
+                    </label>
+                    <input
+                        {...register('material', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.material?.message}</p>
+                </div>
+                <div>
+                    <label className="text-base font-medium leading-none text-gray-800">
+                        Image link
+                    </label>
+                    <input
+                        {...register('img', {
+                            required: { value: true, message: 'This is required!' },
+                        })}
+                        aria-label="enter name"
+                        type="text"
+                        placeholder="https://i.ibb.co/x3py39p/pliers.webp"
+                        className="text-md mt-1 block w-full rounded border border-gray-300 py-2  px-4 shadow-sm focus:outline-primary md:text-lg"
+                    />
+                    <p className="text-red-500">{errors.img?.message}</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center space-x-2">
+                    <button className="border-2 border-primary bg-primary px-7 py-3 font-josefin text-base font-bold text-white transition duration-500 ease-in-out hover:border-primary  hover:bg-transparent hover:text-primary ">
+                        Add
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }
